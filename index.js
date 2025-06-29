9104f80f0a4149c29d63e470e981993b257bc7da
alittaoj <alittaoj30@gmail.com>
2025-06-29 15:04:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mangoose");
+const BlogModel = require("./models/BlogModel");
+
+const app = express();
+var PORT = 3001;
+app.use(express.json());
+app.use(cors());
+//Write missing code here
+
+//Write your POST API here
+
+app.get("/get", async (req, res) => {
+  try {
+    let data = await BlogModel.find();
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`${PORT} is up and running`);
+});
