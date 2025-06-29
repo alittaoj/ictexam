95ba9446fe8d3a48af9f1678a268bd6cf723de8e
alittaoj <alittaoj30@gmail.com>
2025-06-29 19:37:26 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
-//Write missing code here
+
 mongoose
-  .connect(
-   
-  )
+  .connect("mongodb+srv://Angelin:alitta@cluster0.xkndept.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log("Connected to DB");
+    console.log("✅ Connected to MongoDB");
   })
   .catch((error) => {
-    console.log(error);
+    console.log("❌ DB connection error:", error);
   });
+
